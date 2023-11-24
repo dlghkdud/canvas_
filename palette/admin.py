a5fb7e3214d7258317185c86bf8a03092856cd48
dlghkdud <dlflo00f@gmail.com>
2023-11-24 21:25:24 +0900

    reset

--- palette/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Drawing
-
-# Register your models here.
-class DrawingAdmin(admin.ModelAdmin):
-    search_fields = ['subject']
-
-admin.site.register(Drawing, DrawingAdmin)
--- palette/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaletteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'palette'
